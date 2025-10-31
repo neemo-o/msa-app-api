74f610405d8a289286c3f1aeccc6114413537cab
Neemias Vasconcelos <167483565+neemo-o@users.noreply.github.com>
2025-10-31 11:25:16 -0300

    Feature add: Servidor base rodando na porta designada

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,23 @@
-console.log("Testing the server");
+// Imports
+import express from 'express';
+import dotenv from "dotenv";
+
+// Configuration
+dotenv.config();
+
+// Applcation
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+async function startServer() {
+  try {
+    app.listen(PORT, () => {
+      console.log(`Servidor conectado e rodando na porta ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Erro ao conectar ao banco de dados:", error);
+    process.exit(1);
+  }
+}
+
+startServer();
