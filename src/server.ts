d81a2dceead9995c55076e5fdb80ff65adf17a7f
Neemias Vasconcelos <167483565+neemo-o@users.noreply.github.com>
2025-10-31 12:05:10 -0300

    Feature add: Prisma PostgreSQL for database manipulation

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,21 @@
 // Imports
 import express from 'express';
 import dotenv from "dotenv";
+import { PrismaClient } from '@prisma/client';
 
 // Configuration
 dotenv.config();
 
 // Applcation
 const app = express();
+const prisma = new PrismaClient();
 const PORT = process.env.PORT || 3000;
 
 async function startServer() {
   try {
+    await prisma.$connect();
+    console.log("Conectado ao banco de dados");
+
     app.listen(PORT, () => {
       console.log(`Servidor conectado e rodando na porta ${PORT}`);
     });
